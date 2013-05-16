4019dd10e8c8f035002d68353ba78de23a6ae73e
epmclaughlin <epmclaughlin@gmail.com>
2013-05-16 00:37:55 -0400

    refactored simplY_decimal to use decimal_time includes

--- simply_decimal/src/simply_decimal.c
+++ simply_decimal/src/simply_decimal.c
@@ -2,6 +2,7 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 #include "mini-printf.h"
+#include "decimal_time.h"
 
 #define MY_UUID { 0xFC, 0x6A, 0x4A, 0x8B, 0x6A, 0xE3, 0x4B, 0x02, 0x99, 0xA5, 0xF8, 0x7A, 0x37, 0x73, 0xCB, 0x99 }
 PBL_APP_INFO(MY_UUID, "Simply Decimal", "Vuzzbox", 1, 0 /* App version */, RESOURCE_ID_IMAGE_MENU_ICON, APP_INFO_WATCH_FACE);
@@ -70,49 +71,19 @@ void handle_init(AppContextRef ctx) {
   // TODO: Update display here to avoid blank display on launch?
 }
 
-// Return seconds since epoch compensating for Pebble's lack of location independent GMT
-// Thanks: WhyIsThisOpen's Unix Time source: http://forums.getpebble.com/discussion/4324/watch-face-unix-time
-uint32_t get_epoch_seconds(PblTm *current_time) {
-  unsigned int unix_time;
-  /* Convert time to seconds since epoch. */
-  unix_time = ((0-TIME_ZONE_OFFSET)*3600) + /* time zone offset */ 
-            + current_time->tm_sec /* start with seconds */
-            + current_time->tm_min*60 /* add minutes */
-            + current_time->tm_hour*3600 /* add hours */
-            + current_time->tm_yday*86400 /* add days */
-            + (current_time->tm_year-70)*31536000 /* add years since 1970 */
-            + ((current_time->tm_year-69)/4)*86400 /* add a day after leap years, starting in 1973 */
-            - ((current_time->tm_year-1)/100)*86400 /* remove a leap day every 100 years, starting in 2001 */
-            + ((current_time->tm_year+299)/400)*86400; /* add a leap day back every 400 years, starting in 2001*/
-  return unix_time;
-}
-
-void get_decimal_time(char *time_string) {
-  PblTm current_time;
-  unsigned int decimal_seconds_from_midnight;
-  unsigned int archaic_seconds_from_midnight;
-  int decimal_hour;
-  int decimal_minute;
-
-  get_time(&current_time);
-
-  archaic_seconds_from_midnight = get_epoch_seconds(&current_time) % 86400;
-  decimal_seconds_from_midnight = (unsigned int)(archaic_seconds_from_midnight * DECIMAL_TIME_CONVERSION_MULTIPLIER);
-
-  decimal_hour = (int)(decimal_seconds_from_midnight / 10000);
-  decimal_minute = (int)((decimal_seconds_from_midnight % 10000) / 100);
-
-  memset(&time_string[0], 0, sizeof(time_string));
-  snprintf(time_string, 8, "%d:%02d", decimal_hour, decimal_minute);
-}
-
 void handle_timer(AppContextRef ctx, AppTimerHandle handle, uint32_t cookie) {
   (void)ctx;
   (void)handle;
 
   if (cookie == COOKIE_DECIMAL_TIME_TIMER) {
+    PblDeciTm t;
     static char time_string[8];
-    get_decimal_time(time_string);
+
+    get_decimal_time(&t);
+
+    memset(&time_string[0], 0, sizeof(time_string));
+    snprintf(time_string, 8, "%d:%02d", t.tm_hour, t.tm_min);
+
     text_layer_set_text(&text_time_layer, time_string);
     text_layer_set_text(&text_dt_layer, "dt");
 
